871787c70f141fec29c6615b47db2092e68268bd
jayaantara10 <jayaantara10@gmail.com>
2021-11-29 23:07:45 +0800

    Penambahan font halaman list jurnal user dan list jurnal pengunjung

---
+++ app/src/main/java/id/vigyan/rumahjurnal/DaftarJurnalUserActivity.java
@@ -0,0 +1,14 @@
+package id.vigyan.rumahjurnal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DaftarJurnalUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar_jurnal_user);
+    }
+}
