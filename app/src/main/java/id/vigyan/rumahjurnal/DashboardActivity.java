0bce4e702ba617f84feb465d36f55535f66740cb
vigyanmelati <gyanixc11@gmail.com>
2021-12-12 20:46:45 +0800

    Nambah dashboard

---
+++ app/src/main/java/id/vigyan/rumahjurnal/DashboardActivity.java
@@ -0,0 +1,49 @@
+package id.vigyan.rumahjurnal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DashboardActivity extends AppCompatActivity {
+    private CardView daftarJurnalUmum, daftarJurnalAnda, editProfil, tentangKami;
+    private Button logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        logout = findViewById(R.id.logout);
+        daftarJurnalUmum = findViewById(R.id.daftarJurnalUmum);
+        daftarJurnalAnda = findViewById(R.id.daftarJurnalAnda);
+        editProfil = findViewById(R.id.editProfil);
+        tentangKami = findViewById(R.id.tentangKami);
+
+        daftarJurnalUmum.setOnClickListener(clickListener);
+        daftarJurnalAnda.setOnClickListener(clickListener);
+        editProfil.setOnClickListener(clickListener);
+        tentangKami.setOnClickListener(clickListener);
+        logout.setOnClickListener(clickListener);
+    }
+
+    public View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent;
+
+            switch (v.getId()){
+                case R.id.daftarJurnalUmum : intent = new Intent(DashboardActivity.this,DaftarJurnalActivity.class);startActivity(intent);break;
+                case R.id.daftarJurnalAnda : intent = new Intent(DashboardActivity.this,DaftarJurnalUserActivity.class);startActivity(intent);break;
+                case R.id.editProfil : intent = new Intent(DashboardActivity.this,MainActivity.class);startActivity(intent);break;
+                case R.id.tentangKami : intent = new Intent(DashboardActivity.this,MainActivity.class);startActivity(intent);break;
+                case R.id.logout : intent = new Intent(DashboardActivity.this,LoginActivity.class);startActivity(intent);break;
+                default:break;
+            }
+
+        }
+    };
+
+}
--- app/src/main/java/id/vigyan/rumahjurnal/LoginActivity.java
+++ app/src/main/java/id/vigyan/rumahjurnal/LoginActivity.java
@@ -2,13 +2,34 @@ package id.vigyan.rumahjurnal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText login_email, login_pass;
+    private Button btn_login;
+    private TextView tv_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login_email = findViewById(R.id.login_email);
+        login_pass = findViewById(R.id.login_pass);
+        btn_login = findViewById(R.id.btn_login);
+        tv_signup = findViewById(R.id.tv_signup);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getApplicationContext(), DashboardActivity.class);
+                startActivity(intent1);
+            }
+        });
     }
 }
