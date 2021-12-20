d9fe360e30b1434e60363028fd3737d336507a2a
vigyanmelati <gyanixc11@gmail.com>
2021-12-20 20:21:17 +0800

    layout tentang kami, edit profil

---
+++ app/src/main/java/id/vigyan/rumahjurnal/TentangKamiActivity.java
@@ -0,0 +1,14 @@
+package id.vigyan.rumahjurnal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TentangKamiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang_kami);
+    }
+}
